e8f39ac5e6ed9d9092e435d64543e73e92b305e8
ck198 <charmevillanueva1@gmail.com>
2025-03-08 10:46:08 +0800

    Create LQ2_2_VILLANUEVA.js

---
+++ LQ2_2_VILLANUEVA.js
@@ -0,0 +1,7 @@
+// 2. Loop Statement (Repeat Dog Breed Name)
+let dog_breed = prompt("Enter dog breed:");
+let print_count = parseInt(prompt("Enter how many times to print:"));
+
+for (let i = 0; i < print_count; i++) {
+    console.log(dog_breed);
+}
